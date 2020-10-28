abfade774d0c92c7124b781af6e1c4a90e6fea4c
Victor Forbes <vhforbes@gmail.com>
2020-10-28 16:27:34 -0300

    Updates

---
+++ precache-manifest.8ecd5347845fd05446104332cbf19f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6990f35e4b6dd5e341ac3ea1e3863278",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "520191abf1769bdf76c0",
+    "url": "/todo-list-react/static/css/main.336b71b6.chunk.css"
+  },
+  {
+    "revision": "34c1bbfb3efdc5a327d5",
+    "url": "/todo-list-react/static/js/2.83937ea3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.83937ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520191abf1769bdf76c0",
+    "url": "/todo-list-react/static/js/main.93fe89ad.chunk.js"
+  },
+  {
+    "revision": "826642a5112903c57c77",
+    "url": "/todo-list-react/static/js/runtime-main.22aeb5a4.js"
+  }
+]);
